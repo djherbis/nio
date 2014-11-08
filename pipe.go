c19f2d1af7dd7f6e83a9cb39da337e863475fce0
djherbis <djherbis@gmail.com>
2014-11-08 15:26:50 -0500

    Simplified nio to only require buffer

--- pipe.go
+++ pipe.go
@@ -4,7 +4,6 @@ import (
 	"io"
 
 	"github.com/djherbis/buffer"
-	"github.com/djherbis/nio/channel"
 )
 
 type sdReader struct {
@@ -38,50 +37,16 @@ func Pipe(buf ...buffer.Buffer) (io.ReadCloser, io.WriteCloser) {
 
 func Copy(dst io.Writer, src io.Reader, buf ...buffer.Buffer) (n int64, err error) {
 
-	input := make(chan interface{})
-	output := make(chan interface{})
-
 	if len(buf) == 0 {
 		buf = append(buf, buffer.NewUnboundedBuffer(32*1024, 100*1024*1024))
 	}
 
-	pending := NewBufferQueue(buffer.NewMulti(buf...))
+	pending := buffer.NewSync(buffer.NewMulti(buf...))
 
-	go channel.ChanQueue(input, output, pending)
-	go inFeed(src, input)
+	go func() {
+		io.Copy(pending, src)
+		pending.Done()
+	}()
 
-	return outFeed(dst, output)
-}
-
-func inFeed(r io.Reader, in chan<- interface{}) {
-	data := make([]byte, 32*1024)
-	p := NewPacket()
-	for {
-		if n, err := r.Read(data); err == nil {
-			p.data = data[:n]
-			in <- p
-			<-p.ok
-		} else {
-			close(in)
-			return
-		}
-	}
-}
-
-func outFeed(w io.Writer, out <-chan interface{}) (n int64, err error) {
-	data := make([]byte, 32*1024)
-	for output := range out {
-		p := output.(*Packet)
-		x := copy(data, p.data)
-		<-p.ok
-		var m int
-		for len(data[m:x]) > 0 {
-			m, err = w.Write(data[m:x])
-			n += int64(m)
-			if err != nil {
-				return n, err
-			}
-		}
-	}
-	return n, err
+	return io.Copy(dst, pending)
 }
