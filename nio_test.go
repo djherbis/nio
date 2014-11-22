4345ea88ffb16dc0422865e68329e02893a439dd
djherbis <djherbis@gmail.com>
2014-11-21 20:51:38 -0500

    Added Benchmarks

--- nio_test.go
+++ nio_test.go
@@ -2,10 +2,24 @@ package nio
 
 import (
 	"bytes"
+	"crypto/rand"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"testing"
 )
+
+func BenchmarkIOCopy(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		io.Copy(ioutil.Discard, io.LimitReader(rand.Reader, 10*1024*1024))
+	}
+}
+
+func BenchmarkNioCopy(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Copy(ioutil.Discard, io.LimitReader(rand.Reader, 10*1024*1024))
+	}
+}
 
 func TestPipe(t *testing.T) {
 	r, w := Pipe()
