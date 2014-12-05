4effb9a8192980ccda1f561ea3ce7365764ff0df
djherbis <djherbis@gmail.com>
2014-12-04 19:51:26 -0500

    Added ChunkWriter which splits a big Write into fixed-size chunks (except the last)

--- util.go
+++ util.go
@@ -1,5 +1,10 @@
 package nio
 
+import (
+	"fmt"
+	"io"
+)
+
 type WriterFunc func([]byte) (int, error)
 
 func (w WriterFunc) Write(p []byte) (int, error) {
@@ -8,4 +13,37 @@ func (w WriterFunc) Write(p []byte) (int, error) {
 
 func (w WriterFunc) Close() error {
 	return nil
+}
+
+type chunkWriter struct {
+	io.Writer
+	chunk int
+}
+
+func ChunkWriter(w io.Writer, chunk int) io.Writer {
+	return &chunkWriter{
+		Writer: w,
+		chunk:  chunk,
+	}
+}
+
+func (w *chunkWriter) Write(p []byte) (n int, err error) {
+	var m int
+	for len(p[n:]) > 0 {
+
+		if n+w.chunk <= len(p) {
+			m, err = w.Writer.Write(p[n : n+w.chunk])
+			fmt.Println("\n|")
+		} else {
+			m, err = w.Writer.Write(p[n:])
+		}
+
+		n += m
+
+		if err != nil {
+			return n, err
+		}
+
+	}
+	return n, err
 }
