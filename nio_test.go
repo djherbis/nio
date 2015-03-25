d1eec1b981877c58cd8287215e7ed70e024f1243
djherbis <djherbis@gmail.com>
2015-03-24 22:52:35 -0400

    added a simple Copy test

--- nio_test.go
+++ nio_test.go
@@ -7,6 +7,25 @@ import (
 	"github.com/djherbis/buffer"
 )
 
+func TestCopy(t *testing.T) {
+	buf := buffer.New(1024)
+	input := bytes.NewBuffer(nil)
+	output := bytes.NewBuffer(nil)
+
+	input.Write([]byte("hello world"))
+	n, err := Copy(output, input, buf)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if int(n) != len("hello world") {
+		t.Errorf("wrote wrong # of bytes")
+	}
+
+	if !bytes.Equal(output.Bytes(), []byte("hello world")) {
+		t.Errorf("output didn't match: %s", output.Bytes())
+	}
+}
+
 func TestPipe(t *testing.T) {
 	buf := buffer.New(1024)
 	r, w := Pipe(buf)
